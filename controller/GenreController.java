bf40889d468fede297a01c722a8a742a5f2ba905
bhargav <bhrgvkrishna354@gmail.com>
2022-02-01 15:19:38 +0000

    updated GenreController with proper API requests

--- controller/GenreController.java
+++ controller/GenreController.java
@@ -1,7 +1,7 @@
 package com.Bhargav.libraryManagement.controller;
 
 import com.Bhargav.libraryManagement.model.Genre;
-import com.Bhargav.libraryManagement.repository.GenreRepository;
+import com.Bhargav.libraryManagement.service.GenreService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -15,15 +15,16 @@ import java.util.Optional;
 public class GenreController {
 
     @Autowired
-    private GenreRepository genreRepository;
+    private GenreService genreService;
 
     @GetMapping
-    public List<Genre> findAllBooks(){
-        return genreRepository.findAll();
+    public List<Genre> findAllGenre(){
+
+        return genreService.findAllGenres();
     }
-    @GetMapping("/{id}")
+    @GetMapping("book/{id}")
     public ResponseEntity<Genre> findBookById(@PathVariable(value = "id") long id) {
-        Optional<Genre> genre = genreRepository.findById(id);
+        Optional<Genre> genre = genreService.findGenreById(id);
         if (genre.isPresent()){
             return ResponseEntity.ok().body(genre.get());
         }
@@ -32,8 +33,18 @@ public class GenreController {
         }
     }
 
-    @PostMapping
-    public Genre saveBook(@Validated @RequestBody Genre genre) {
-        return genreRepository.save(genre);
+    @PostMapping("/add-Genre")
+    public void saveGenre(@Validated @RequestBody Genre genre) {
+        genreService.createGenre(genre);
+    }
+
+    @PutMapping("/update-Genre/{id}")
+    public void updateGenre(@PathVariable(value="id") Long id, @Validated @RequestBody Genre genre){
+        Optional<Genre> genreById = genreService.findGenreById(id);
+        genreService.updateGenre(genre);
+    }
+    @DeleteMapping("/remove-Genre/{id}")
+    public void deleteGenre(@PathVariable(value = "id") Long id){
+        genreService.deleteGenre(id);
     }
 }
